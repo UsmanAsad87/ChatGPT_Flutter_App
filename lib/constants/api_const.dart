2670c296c2a34b5b343f7c58ca0e283f5da25fd3
usmanasad87 <usmanasad0324@gmail.com>
2024-04-24 21:50:58 +0500

    api key updaed

--- lib/constants/api_const.dart
+++ lib/constants/api_const.dart
@@ -1,3 +1,5 @@
 String baseUrlModels='https://api.openai.com/v1';
 
-String YOUR_API_KEY="sk-proj-75MTBbO3cX0psGWPyYHAT3BlbkFJ4j7Rny2eV3zHsiwN7epu";
+//String YOUR_API_KEY="sk-proj-
+// 75MTBbO3cX0psGWPyYHAT
+// 3BlbkFJ4j7Rny2eV3zHsiwN7epu";
